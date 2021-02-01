35c27d2a6c5dd9165f9843e08b64c4e3fa82a620
Uriegas <1930526@upv.edu.mx>
2021-02-01 10:28:42 -0600

    New Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RickymortyComponent } from './rickymorty/rickymorty.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RickymortyComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/rickymorty/rickymorty.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rickymorty',
+  templateUrl: './rickymorty.component.html',
+  styleUrls: ['./rickymorty.component.sass']
+})
+export class RickymortyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
