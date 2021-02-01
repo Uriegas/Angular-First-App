e9977b6698f204b472528e96b58d5c0ef7ef71a0
Uriegas <1930526@upv.edu.mx>
2021-02-01 11:52:06 -0600

    Added Modules and Libraries

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'home', loadChildren: () => import('./component/pages/home/home.module').then(m => m.HomeModule) }, { path: 'character-list', loadChildren: () => import('./component/pages/characters/character-list/character-list.module').then(m => m.CharacterListModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RickymortyComponent } from './rickymorty/rickymorty.component';
+import { HeaderComponent } from './shared/component/header/header.component';
+import { FormSearchComponent } from './shared/component/form-search/form-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RickymortyComponent
+    RickymortyComponent,
+    HeaderComponent,
+    FormSearchComponent
   ],
   imports: [
     BrowserModule,
