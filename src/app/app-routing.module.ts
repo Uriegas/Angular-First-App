aba91d20879e90cef9e7a43ac316dbb0ae159b7b
Uriegas <1930526@upv.edu.mx>
2021-02-13 20:28:58 -0600

    Load from Home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CharacterListModule } from './component/pages/characters/character-list/character-list.module';
 
-const routes: Routes = [{ path: 'home', loadChildren: () => import('./component/pages/home/home.module').then(m => m.HomeModule) }, { path: 'character-list', loadChildren: () => import('./component/pages/characters/character-list/character-list.module').then(m => m.CharacterListModule) }];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./component/pages/characters/character-list/character-list.module').then(m => m.CharacterListModule) },
+  { path: 'character-list', loadChildren: () => import('./component/pages/characters/character-list/character-list.module').then(m => m.CharacterListModule) },
+  { path: 'home', loadChildren: () => import('./component/pages/home/home.module').then(m => m.HomeModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
